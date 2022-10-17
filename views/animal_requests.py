2a92385cf0242c94802ab15472af25dac2ccaa5f
Marcus-Cox <marcus.cox92@gmail.com>
2022-10-17 14:25:48 -0500

    book 2

---
+++ views/animal_requests.py
@@ -0,0 +1,34 @@
+#Constant named ANIMALS
+ANIMALS = [
+    {
+        # In Python, this is a list of dictionaries
+        "id": 1,
+        "name": "Snickers",
+        "species": "Dog",
+        "locationId": 1,
+        "customerId": 4
+    },
+    {
+        "id": 2,
+        "name": "Roman",
+        "species": "Dog",
+        "locationId": 1,
+        "customerId": 2
+    },
+    {
+        "id": 3,
+        "name": "Blue",
+        "species": "Cat",
+        "locationId": 2,
+        "customerId": 1
+    }
+]
+
+
+def get_all_animals():
+    """function to get all animals in the list of dictionaries
+
+    Returns:
+        _type_: ANIMALS
+    """
+    return ANIMALS
